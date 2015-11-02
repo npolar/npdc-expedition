8ec9e341e4e158a783df6deda2aabcebdbba4ca3
RDuX <ruben.dens@gmail.com>
2015-11-02 14:14:10 +0100

    use npdcAppConf for configuration

--- src/show/ExpeditionShowController.js
+++ src/show/ExpeditionShowController.js
@@ -1,16 +1,19 @@
 'use strict';
 /**
- * 
+ *
  *
  * @ngInject
  */
-var ExpeditionShowController = function ($scope, $controller, $routeParams, Expedition, NpolarApiSecurity) {
-  
+var ExpeditionShowController = function ($scope, $controller, $routeParams, Expedition, NpolarApiSecurity, npdcAppConfig) {
+
   $controller('NpolarBaseController', {$scope: $scope});
   $scope.resource = Expedition;
-  $scope.security = NpolarApiSecurity;  
+  $scope.security = NpolarApiSecurity;
   $scope.show();
-  
+
+  $scope.show().$promise.then(data => {
+    npdcAppConfig.cardTitle = data.code;
+  });
 };
 
 module.exports = ExpeditionShowController;
