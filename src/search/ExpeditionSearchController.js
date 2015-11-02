ef3e23048c5a146486a7ef6ddf266bfd8b34df3f
RDuX <ruben.dens@gmail.com>
2015-11-02 11:45:21 +0100

    update search controller with npdcAppConfig

--- src/search/ExpeditionSearchController.js
+++ src/search/ExpeditionSearchController.js
@@ -3,20 +3,25 @@ var angular = require('angular');
 /**
  * @ngInject
  */
-var ExpeditionSearchController = function ($scope, $location, $controller, Expedition) {
+var ExpeditionSearchController = function ($scope, $location, $controller, Expedition, npdcAppConfig) {
 
   $controller('NpolarBaseController', { $scope: $scope });
   $scope.resource = Expedition;
-    
+
   let query = function() {
-    
-    let defaults = { limit: "all", sort: "-updated", fields: 'title,id,updated' };
+
+    let defaults = { limit: "all", sort: "-updated", fields: 'code,id,updated,type' };
     let invariants = $scope.security.isAuthenticated() ? {} : {} ;
-    
+
     return angular.extend(defaults, $location.search(), invariants);
   };
 
+  npdcAppConfig.cardTitle = "Expeditions";
   $scope.search(query());
+
+  npdcAppConfig.search.results = {
+    subtitle: "type"
+  };
 
 };
 
