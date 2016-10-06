3f70e0be8bc4a932d464d51db04102a8fdb10897
srldl <siri.uldal@gmail.com>
2016-10-06 10:33:59 +0200

    updated show

--- src/show/ExpeditionShowController.js
+++ src/show/ExpeditionShowController.js
@@ -18,17 +18,6 @@ var ExpeditionShowController = function ($scope, $controller, $q, $routeParams,
   };
 
 
-  let uri = (expedition) => {
-    let link = expedition.links.find(l => {
-      return l.rel === "alternate" && (/html$/).test(l.type);
-    });
-    if (link) {
-      return link.href.replace(/^http:/, "https:");
-    } else {
-      return `https://data.npolar.no/expedition/${ expedition.id }`;
-    }
-  };
-
   $scope.mapOptions = {};
 
   let show = function() {
@@ -36,23 +25,14 @@ var ExpeditionShowController = function ($scope, $controller, $q, $routeParams,
 
 
       if (expedition.locations) {
-
          let bounds = (expedition.locations).map((locations) => [[locations.south, locations.west], [locations.north, locations.east]]);
          $scope.mapOptions.coverage = bounds;
          $scope.mapOptions.geojson = "geojson";
-
-
       }
-      $scope.mapOptions.geometries = expedition.links.filter(l => l.type === "application/vnd.geo+json").map(l => l.href);
-
-
-
-      $scope.links = expedition.links.filter(l => (l.rel !== "alternate" && l.rel !== "edit") && l.rel !== "data");
-
 
       //Convert from camelCase to human readable
-      for(var j=0; j<($scope.document.research).length; j++){
-         $scope.document.research[j].research_type = convert($scope.document.research[j].research_type);
+      for(var j=0; j<($scope.document.activity).length; j++){
+         $scope.document.activity[j].activity_type = convert($scope.document.activity[j].activity_type);
       }
 
       //Convert from camelCase to human readable
@@ -60,45 +40,6 @@ var ExpeditionShowController = function ($scope, $controller, $q, $routeParams,
       	 for(var k=0; k<($scope.document.people[i].roles).length; k++){
          $scope.document.people[i].roles[k] = convert($scope.document.people[i].roles[k]);
       }}
-
-
-      $scope.alternate = expedition.links.filter(l => ((l.rel === "alternate" && l.type !== "text/html") || l.rel === "edit")).concat({
-        href: `http://api.npolar.no/expedition/?q=&filter-id=${expedition.id}&format=json&variant=ld`,
-        title: "DCAT (JSON-LD)",
-        type: "application/ld+json"
-      });
-
-
-      $scope.uri = uri(expedition);
-
-
-
-   /*  let relatedDatasets = Dataset.array({
-        q: Expedition.title,
-        fields: 'id,title,collection',
-        score: true,
-        limit: 5,
-        'not-id': Expedition.id,
-        op: 'OR'
-      }).$promise;
-      let relatedPublications = Publication.array({
-        q: Expedition.title,
-        fields: 'id,title,published_sort,collection',
-        score: true,
-        limit: 5,
-        op: 'OR'
-      }).$promise;
-      let relatedProjects = Project.array({
-        q: Expedition.title,
-        fields: 'id,title,collection',
-        score: true,
-        limit: 5,
-        op: 'OR'
-      }).$promise;
-
-      $q.all([relatedDatasets, relatedPublications, relatedProjects]).then(related => {
-        $scope.related = related;
-      }); */
 
     });
 
